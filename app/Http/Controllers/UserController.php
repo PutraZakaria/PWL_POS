07e891ac6cd522b8af452f34cd60b055f34a0079
PutraZakaria <p1578003@gmail.com>
2024-03-03 20:47:23 +0700

    Praktikum 6

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(){
+        //  tambah data user dengan Eloquent Model-1 (Hasil error ngga ada data level_id 4) - Tapi bisa diatasi dengan menambah level
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4,
+        // ];
+        // UserModel::insert($data); // tambahkan data ke tabel m_users
+
+        //  tambah data user dengan Eloquent Model-2
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //Update data user
+
+        // coba akses model UserModel
+        $user = UserModel::all();   //ambil semua data dari tabel m_users
+        return view('user', ['data' => $user]);
+    }
+}
