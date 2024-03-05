310f556f7314c1aa17bbe57450e6462b613d986b
PutraZakaria <p1578003@gmail.com>
2024-03-05 09:16:42 +0700

    Praktikum 1 - $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,13 +19,25 @@ class UserController extends Controller
         // UserModel::insert($data); // tambahkan data ke tabel m_users
 
         //  tambah data user dengan Eloquent Model-2
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); //Update data user
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); //Update data user
 
         // coba akses model UserModel
-        $user = UserModel::all();   //ambil semua data dari tabel m_users
+        // $user = UserModel::all();   //ambil semua data dari tabel m_users
+        // return view('user', ['data' => $user]);
+
+        // JOBSHEET 4
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
