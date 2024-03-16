38469d7a65ca7983279c778f527354272af3b607
PutraZakaria <p1578003@gmail.com>
2024-03-17 05:26:36 +0700

    Praktikum 5-Soal 1

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -11,6 +11,8 @@
         <div class="card">
             <div class="card_header"></div>
             <div class="card-body">
+                {{-- Praktikum 5-Soal 1 --}}
+                <a href="{{ url('kategori/create') }}" class="btn btn-primary" style="float: right">Add</a>
                 {{$dataTable->table()}}
             </div>
         </div>
