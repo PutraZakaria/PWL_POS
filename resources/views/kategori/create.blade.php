d37135ba4911a0bbca9e949231c962ba4e5520fc
PutraZakaria <p1578003@gmail.com>
2024-03-20 06:16:01 +0700

    B. VALIDASI PADA SERVER

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -13,11 +13,28 @@
                 <h3 class="card-title">Buat kategori baru</h3>
             </div>
 
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+
             <form method="post" action="../kategori">
                 <div class="card-body">
                     <div class="form-group">
-                        <label for="kodeKategori">Kode Kategori</label>
-                        <input type="text" class="form-control" id="kodeKategori" name="kodeKategori">
+                        <label for="kategori_kode">Kode Kategori</label>
+                        <input id="kategori_kode" type="text" name="kategori_kode" class="@error('kategori_kode') is-invalid @enderror form-control">
+
+                        @error('kategori_kode')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                        {{-- <label for="kodeKategori">Kode Kategori</label>
+                        <input type="text" class="form-control" id="kodeKategori" name="kodeKategori"> --}}
                     </div>
                     <div class="form-group">
                         <label for="namaKategori">Nama Kategori</label>
@@ -33,20 +50,5 @@
     </div>
 @endsection
 
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
 
 
-<label for="kategori_kode">Kode Kategori</label>
-<input id="kategori_kode" type="text" name="kategori_kode" class="@error('kategori_kode') is-invalid @enderror">
-
-@error('kategori_kode')
-    <div class="alert alert-danger">{{ $message }}</div>
-@enderror
