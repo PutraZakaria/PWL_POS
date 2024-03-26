01b4aec4ae5c146f37e34b12337b0ccd97d52b61
PutraZakaria <p1578003@gmail.com>
2024-03-26 11:02:26 +0700

    Jobsheet 7: Praktikum 1 – Layouting AdminLTE

--- resources/views/m_level/index.blade.php
+++ resources/views/m_level/index.blade.php
@@ -23,17 +23,17 @@
         <th width="20px" class="text-center">ID</th>
         <th width="150px" class="text-center">Kode Level</th>
         <th width="200px" class="text-center">Nama Level</th>
-        {{-- <th width="15px" class="text-center">Action</th> --}}
+        <th width="15px" class="text-center">Action</th>
     </tr>
     @foreach ($data as $d)
         <tr>
             <td>{{ $d->level_id}}</td>
             <td>{{ $d->level_kode}}</td>
             <td>{{ $d->level_nama}}</td>
-            {{-- <td class="m auto">
+            <td class="m auto">
                 <a class="btn btn-primary btn-sm" >Edit</a>
                 <a class="btn btn-danger btn-sm" >Hapus</a>
-            </td> --}}
+            </td>
         </tr>
     @endforeach
 </table>
